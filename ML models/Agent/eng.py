172a8cd42338a96bfff0c9daf3c70bb7432105e0
Ayaan5711 <ahmedayaan570@gmail.com>
2024-11-20 22:12:19 +0530

    changes

---
+++ ML models/Agent/eng.py
@@ -0,0 +1,7 @@
+import  vertexai
+import streamlit as st
+from vertex.generative_models import GenerativeModel 
+# import vertex.ai.preview.generative_models generative_model
+import vertex.ai.preview.generative_models as generative_model
+
+
--- ML models/End-to-End Chatbot Gemini/chat.py
+++ ML models/End-to-End Chatbot Gemini/chat.py
@@ -29,7 +29,7 @@ def get_gemini_response(question):
 
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Q&A Demo")
+st.set_page_config(page_title="Q&A")
 
 st.header("Gemini Application")
 
